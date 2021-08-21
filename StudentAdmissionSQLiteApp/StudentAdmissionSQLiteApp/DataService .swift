948e888fc3c675143a73b7c2c7e1fd5cb31bd586
SHREYA GANDHI <83833264+SHREYUGANDHI@users.noreply.github.com>
2021-08-21 21:32:19 +0530

    Add files via upload

---
+++ StudentAdmissionSQLiteApp/StudentAdmissionSQLiteApp/DataService .swift
@@ -0,0 +1,23 @@
+//
+//  DataService .swift
+//  StudentAdmissionSQLiteApp
+//
+//  Created by DCS on 18/08/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import Foundation
+class DataService {
+    
+    static func getDocDir() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        print("doc dir : \(paths[0])")
+        return paths[0]
+        
+        
+        
+        
+    }
+    
+}
---
+++ StudentAdmissionSQLiteApp/StudentAdmissionSQLiteApp/class StudentVC: UIViewController { class StudentVC: UIViewController { StudentVC.swift
@@ -0,0 +1,30 @@
+//
+//  class StudentVC: UIViewController { class StudentVC: UIViewController { StudentVC.swift
+//  StudentAdmissionSQLiteApp
+//
+//  Created by DCS on 18/08/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import UIKit
+
+class class_StudentVC__UIViewController___class_StudentVC__UIViewController___StudentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
